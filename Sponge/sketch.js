e667c5907a80852b1cc5d99eb7c1e861c3756ee1
VinRie <Vincent.Riesop@gmail.com>
2016-10-08 18:54:20 +0200

    ..

--- Sponge/sketch.js
+++ Sponge/sketch.js
@@ -1,18 +1,4 @@
-var x = 0;
-var a = 0;
-
-var sponge = [];
-var myBox;
-
-function setup() {
-  createCanvas(1200, 800, WEBGL);
-  myBox = new Box(0,0,0,500);
-
-  sponge.push(myBox);
-}
-
-
-function mousePressed(){var x;
+var x;
 var a;
 
 var sponge = [];
@@ -26,6 +12,7 @@ function setup() {
 
   sponge.push(myBox);
 }
+
 
 function mousePressed(){
   clear();
@@ -48,35 +35,6 @@ function draw() {
   //rotateY(a );
   //rotateZ(a * 0.1);
 
-  for(index = 0; index < sponge.length; ++index){
-    sponge[index].draw();
-
-  }
-  a = (a +0.01) % (2* PI) ;
-
-  //myBox.draw();
-}
-
-  clear();
-  var nextGen = new Array();
-
-  for(index = 0; index < sponge.length; ++index){
-    nextGen.push.apply(nextGen, sponge[index].generate());
-  }
-
-  sponge = nextGen;
-
-
-}
-
-function draw() {
-  background(100);
-  var index;
-
-  //rotateX(a);
-  //rotateY(a );
-  //rotateZ(a * 0.1);
- // schnucki
   for(index = 0; index < sponge.length; ++index){
     sponge[index].draw();
 
