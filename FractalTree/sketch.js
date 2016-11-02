d864bdbd8485fe519aa5c311e7a5954339858ca4
Vincent Riesop <vincent.riesop@gmail.com>
2016-11-02 23:15:39 +0100

    angle changed

--- FractalTree/sketch.js
+++ FractalTree/sketch.js
@@ -8,22 +8,22 @@ let colorB;
 function setup(){
   createCanvas(width,height)
   // Beautiful trianglish  fractal
-  // angle = PI*4 / sin(1);
+   angle = PI*4 / sin(1);
 
   // angle = PI / sin(PI);
-    angle = PI / 2;
+    //angle = PI / 2;
 }
 
 
 function draw(){
-  angle = PI / (((mouseX+width/2) / width) * 4)
-  colorR = ((mouseY) / width*2) * 255
+  //angle = PI / (((mouseX+width/2) / width) * 4)
+  colorR = mouseX / width *  255
   colorG = (mouseX / width * 2) * 255
   colorB = (mouseX/ width) * 255
 
   background(51)
   stroke(colorR, colorG, 0)
-  strokeWeight(2);
+  strokeWeight(1);
   translate(width/2,height)
   branch(250)
  
@@ -31,19 +31,19 @@ function draw(){
 }
 
 function branch(len){
-  if(len < 10)
+  if(len < 1)
     return
   line(0,0,0,-len)
   push()
     translate(0, -len)
     rotate(angle)
-    branch(len*0.75)
+    branch(len*0.6)
   pop()
 
   push()
     translate(0, -len)
     rotate(-angle)
-    branch(len*0.6)
+    branch(len*0.7)
   pop()
 
 
